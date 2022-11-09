29e7ade537391840e7933764f07a6b7377c38da8
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-11-09 16:52:48 -0500

    ⚡ [UPDATE] Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Footer(){
     return(
         <footer className="">
-            <div className="container px-4 h-16 flex items-center justify-center">
-                All rights reserved 2022
+            <div className="container px-4 h-16 flex gap-2 items-center justify-center">
+                Coded by <a href="https://github.com/jsonfm/pokedex" target="_blank" rel="noopener noreferrer"><b className="text-rose-600">@jsonfm</b></a>
             </div>
         </footer>
     )
